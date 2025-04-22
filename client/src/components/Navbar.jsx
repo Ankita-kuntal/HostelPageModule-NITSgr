ec938c94aaa97344ccd40329bfc5b8c678511719
Asma <aa3corner@gmail.com>
2025-04-22 18:01:08 +0530

    Common Hostel Link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,9 +3,7 @@ import { useState } from "react";
 import logo from "../assets/images/logo-white.png";
 
 const Navbar = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [showAntiRaggingDropdown, setShowAntiRaggingDropdown] = useState(false);
-  const [showRulesDropdown, setShowRulesDropdown] = useState(false); // State for Rules dropdown
+  const [activeDropdown, setActiveDropdown] = useState(null);
   const [showContactModal, setShowContactModal] = useState(false);
 
   const handleOutsideClick = (e) => {
@@ -25,46 +23,34 @@ const Navbar = () => {
           </Link>
         </li>
 
-        {/* Admin */}
+        {/* Hostel Administration Dropdown */}
         <li
-          onMouseEnter={() => setShowDropdown(true)}
-          onMouseLeave={() => setShowDropdown(false)}
+          onMouseEnter={() => setActiveDropdown("admin")}
+          onMouseLeave={() => setActiveDropdown(null)}
           className="relative"
         >
           <Link to="/hostel-admin" className="hover:underline">
             Hostel Administration ▾
           </Link>
-          {showDropdown && (
+          {activeDropdown === "admin" && (
             <ul className="absolute top-full left-0 bg-white text-black rounded-md shadow-md z-10 w-48">
               <li>
-                <a
-                  href="/hostel-admin#dean-welfare"
-                  className="block px-4 py-2 hover:bg-gray-100"
-                >
+                <a href="/hostel-admin#dean-welfare" className="block px-4 py-2 hover:bg-gray-100">
                   Dean Students Welfare
                 </a>
               </li>
               <li>
-                <a
-                  href="/hostel-admin#associate-dean"
-                  className="block px-4 py-2 hover:bg-gray-100"
-                >
+                <a href="/hostel-admin#associate-dean" className="block px-4 py-2 hover:bg-gray-100">
                   Associate Dean
                 </a>
               </li>
               <li>
-                <a
-                  href="/hostel-admin#wardens"
-                  className="block px-4 py-2 hover:bg-gray-100"
-                >
+                <a href="/hostel-admin#wardens" className="block px-4 py-2 hover:bg-gray-100">
                   Wardens
                 </a>
               </li>
               <li>
-                <a
-                  href="/hostel-admin#hall-assistants"
-                  className="block px-4 py-2 hover:bg-gray-100"
-                >
+                <a href="/hostel-admin#hall-assistants" className="block px-4 py-2 hover:bg-gray-100">
                   Hall Assistants
                 </a>
               </li>
@@ -72,42 +58,49 @@ const Navbar = () => {
           )}
         </li>
 
-        {/* Hostel */}
-        <li>
-          <Link to="/girls-hostel" className="hover:underline">
-            Girls Hostel
-          </Link>
-        </li>
-        <li>
-          <Link to="/boys-hostel" className="hover:underline">
-            Boys Hostel
-          </Link>
+        {/* Hostel Info Dropdown */}
+        <li
+          onMouseEnter={() => setActiveDropdown("hostel")}
+          onMouseLeave={() => setActiveDropdown(null)}
+          className="relative"
+        >
+          <span className="cursor-pointer hover:underline">
+            Hostel Info ▾
+          </span>
+          {activeDropdown === "hostel" && (
+            <ul className="absolute top-full left-0 bg-white text-black rounded-md shadow-md z-10 w-48">
+              <li>
+                <Link to="/girls-hostel" className="block px-4 py-2 hover:bg-gray-100">
+                  Girls Hostel
+                </Link>
+              </li>
+              <li>
+                <Link to="/boys-hostel" className="block px-4 py-2 hover:bg-gray-100">
+                  Boys Hostel
+                </Link>
+              </li>
+            </ul>
+          )}
         </li>
 
-        {/* Anti Ragging */}
+        {/* Anti Ragging Dropdown */}
         <li
-          onMouseEnter={() => setShowAntiRaggingDropdown(true)}
-          onMouseLeave={() => setShowAntiRaggingDropdown(false)}
+          onMouseEnter={() => setActiveDropdown("antiRagging")}
+          onMouseLeave={() => setActiveDropdown(null)}
           className="relative"
         >
           <Link to="/anti-ragging/rules" className="hover:underline">
             Anti-Ragging ▾
           </Link>
-          {showAntiRaggingDropdown && (
-            <ul className="absolute top-full left-0 bg-white text-black rounded-md shadow-md z-10 w-48">
+          {activeDropdown === "antiRagging" && (
+            <ul className="absolute top-full left-0 bg-white text-black rounded-md shadow-md z-10 w-56">
               <li>
-                <a
-                  href="/anti-ragging/committee"
-                  className="block px-4 py-2 hover:bg-gray-100"
-                >
+                <a href="/anti-ragging/committee" className="block px-4 py-2 hover:bg-gray-100">
                   Anti Ragging Committee
                 </a>
               </li>
               <li>
-                <a
-                  href="/anti-ragging/rules"
-                  className="block px-4 py-2 hover:bg-gray-100"
-                >
+                <a href="/anti-ragging/rules" className="block px-4 py-2 hover:bg-gray-100">
                   Anti Ragging Rules, Regulations and Undertakings
                 </a>
               </li>
@@ -117,28 +110,22 @@ const Navbar = () => {
 
         {/* Rules Dropdown */}
         <li
-          onMouseEnter={() => setShowRulesDropdown(true)}
-          onMouseLeave={() => setShowRulesDropdown(false)}
+          onMouseEnter={() => setActiveDropdown("rules")}
+          onMouseLeave={() => setActiveDropdown(null)}
           className="relative"
         >
           <Link to="/hostel-rules" className="hover:underline">
             Rules ▾
           </Link>
-          {showRulesDropdown && (
+          {activeDropdown === "rules" && (
             <ul className="absolute top-full left-0 bg-white text-black rounded-md shadow-md z-10 w-48">
               <li>
-                <Link
-                  to="/hostel-rules-summary"
-                  className="block px-4 py-2 hover:bg-gray-100"
-                >
+                <Link to="/hostel-rules-summary" className="block px-4 py-2 hover:bg-gray-100">
                   Summary
                 </Link>
               </li>
               <li>
-                <Link
-                  to="/hostel-rules"
-                  className="block px-4 py-2 hover:bg-gray-100"
-                >
+                <Link to="/hostel-rules" className="block px-4 py-2 hover:bg-gray-100">
                   Detailed Rules
                 </Link>
               </li>
@@ -152,21 +139,20 @@ const Navbar = () => {
             Downloads
           </Link>
         </li>
+
+        {/* Contact Us */}
         <li>
-          {/* Contact Us */}
-          <button
-            onClick={() => setShowContactModal(true)}
-            className="hover:underline"
-          >
+          <button onClick={() => setShowContactModal(true)} className="hover:underline">
             Contact Us
           </button>
         </li>
-        {/* WebTeam */}
-      <li>
-        <Link to="/webteam" className="hover:underline">
-          Web Team
-        </Link>
-      </li>
+
+        {/* Web Team */}
+        <li>
+          <Link to="/webteam" className="hover:underline">
+            Web Team
+          </Link>
+        </li>
       </ul>
 
       {/* Contact Us Modal */}
@@ -177,7 +163,6 @@ const Navbar = () => {
           onClick={handleOutsideClick}
         >
           <div className="bg-white rounded-lg shadow-lg p-6 relative max-w-lg w-full overflow-y-auto max-h-[90vh]">
-            {/* Cross Button */}
             <button
               onClick={() => setShowContactModal(false)}
               className="absolute top-2 right-2 text-gray-900 hover:text-gray-700 font-bold text-xl"
@@ -188,19 +173,13 @@ const Navbar = () => {
             <ul className="space-y-4 text-gray-900">
               <li>
                 <strong>Dean Students Welfare:</strong> +91-6005103740,{" "}
-                <a
-                  href="mailto:deanstudentswelfare@nitsri.ac.in"
-                  className="text-teal-600 underline"
-                >
+                <a href="mailto:deanstudentswelfare@nitsri.ac.in" className="text-teal-600 underline">
                   deanstudentswelfare@nitsri.ac.in
                 </a>
               </li>
               <li>
                 <strong>Associate Dean:</strong> +91-8986948677,{" "}
-                <a
-                  href="mailto:shashikantkumar@nitsri.ac.in"
-                  className="text-teal-600 underline"
-                >
+                <a href="mailto:shashikantkumar@nitsri.ac.in" className="text-teal-600 underline">
                   shashikantkumar@nitsri.ac.in
                 </a>
               </li>
@@ -211,8 +190,6 @@ const Navbar = () => {
           </div>
         </div>
       )}
-
-      
     </nav>
   );
 };
